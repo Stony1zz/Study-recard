e7342fd55bde8e1ad2ee762f369dcfa2dea383b5
Stony1zz <619362860@qq.com>
2018-12-12 13:49:59 +0800

    updata

--- venv/Include/tubao_fenzhi.py
+++ venv/Include/tubao_fenzhi.py
@@ -1,104 +1,194 @@
-# -*- coding: utf-8 -*-
-import turtle
+# 递归法求解凸包
 import random
-import time
-
-f = open('point.txt', 'w')
-for i in range(100):
-    x = random.randrange(-250, 250, 10)
-    y = random.randrange(-200, 200, 10)
-    f.write(str(x) + ',' + str(y) + '\n')
-f.close()
-point = []
-
-f = open('point.txt')
-for i in f:
-    try:
-        temp = i.split(',')
-        x = float(temp[0]);
-        y = float(temp[1])
-        point.append((x, y))
-    except:
-        print
-        'a err'
-f.close()
-
-point = list(set(point))  # 去除重复的点
-
-n = 0
-for i in range(len(point)):
-    if point[n][1] > point[i][1]:
-        n = i
-
-p0 = point[n]
-point.pop(n)
-
-
-def compare(a, b):
-    x = [a[0] - p0[0], a[1] - p0[1]]
-    y = [b[0] - p0[0], b[1] - p0[1]]
-    dx = (x[0] ** 2 + x[1] ** 2) ** 0.5
-    dy = (y[0] ** 2 + y[1] ** 2) ** 0.5
-    cosa = x[0] / dx
-    cosb = y[0] / dy
-    if cosa < cosb:
-        return 1
-    elif cosa > cosb:
-        return -1
-    else:
-        if dx < dy:
-            return -1
-        elif dx > dy:
-            return 1
+import matplotlib.pyplot as plt
+def calTri(p1, p2, p3):
+    size = p1[0] * p2[1] + p2[0] * p3[1] + p3[0] * p1[1] - p3[0] * p2[1] - p2[0] * p1[1] - p1[0] * p3[1]
+    return size
+def maxSize(seq, dot1, dot2, dotSet):
+    maxSize = float('-inf')
+    maxDot = ()
+    online = []
+    maxSet = []
+    for u in seq:
+        size = calTri(dot1, dot2, u)
+        # 判断点u是否能是三角形u dot1 dot2 的面积为正
+        if size < 0:
+            continue
+        elif size == 0:
+            online.append(u)
+        # 若面积为正，则判断是否是距离直线最远的点
+        if size > maxSize:
+            if len(maxDot) > 0:
+                maxSet.append(maxDot)
+            maxSize = size
+            maxDot = u
         else:
-            return 0
+            maxSet.append(u)
+    if not maxSet:
+        # 没找到分割点,同时可能有点落在直线dot1 dot2上
+        if not maxDot:
+            dotSet.extend(online)
+            return [], ()
+        # 有分割点
+        else:
+            dotSet.append(maxDot)
+            return [], maxDot
+    # maxSet不为空
+    else:
+        dotSet.append(maxDot)
+        return maxSet, maxDot
+def minSize(seq, dot1, dot2, dotSet):
+    minSize = float('inf')
+    minDot = ()
+    online = []
+    minSet = []
+    for u in seq:
+        size = calTri(dot1, dot2, u)
+        # 判断点u是否能是三角形u dot1 dot2 的面积为负
+        if size > 0:
+            continue
+        elif size == 0:
+            online.append(u)
+        # 若面积为负，则判断是否是距离直线最远的点
+        if size < minSize:
+            if len(minDot) > 0:
+                minSet.append(minDot)
+            minDot = u
+            minSize = size
+        else:
+            minSet.append(u)
+    if not minSet:
+        # 没找到分割点,同时可能有点落在直线dot1 dot2上
+        if not minDot:
+            dotSet.extend(online)
+            return [], ()
+        # 有分割点
+        else:
+            dotSet.append(minDot)
+            return [], minSet
+    # maxSet不为空
+    else:
+        dotSet.append(minDot)
+        return minSet, minDot
 
 
-point.sort(compare)
-point.insert(0, p0)
-ep = point[:]  # 复制元素，操作ep不会对point产生影响
-tag = 0
-while tag == 0:
-    tag = 1
-    l = len(ep)
-    for i in range(l):
-        i1, i2, i3 = (i, (i + 1) % l, (i + 2) % l)
-        x, y, z = (ep[i1], ep[i2], ep[i3])
-        a1, a2 = ((y[0] - x[0], y[1] - x[1]), (z[0] - y[0], z[1] - y[1]))
-        if a1[0] * a2[1] - a1[1] * a2[0] < 0:
-            tag = 0
-            ep.pop(i2)
-            break
-        elif a1[0] * a2[1] - a1[1] * a2[0] == 0 and a1[0] * a2[0] < 0:
-            # ==0应改写,360度的情况
-            tag = 0
-            ep.pop(i2)
-            break
+# 上包的递归划分
+def divideUp(seq, dot1, dot2, dot3, dot4, dotSet=None):
+    print(dot1, dot2, dot3, dot4)
+    # 初始化第一次运行时的参数
+    if len(seq) == 0:
+        return dotSet
+    if dotSet is None:
+        dotSet = []
+    if len(seq) == 1:
+        dotSet.append(seq[0])
+        return dotSet
+    leftSet, rightSet = [], []
+    # 划分上包左边的点集
+    leftSet, maxDot = maxSize(seq, dot1, dot2, dotSet)
+    # ----------------------------------------------------------------------
+
+    # 对上包左包的点集进一步划分
+    if leftSet:
+        divideUp(leftSet, dot1, maxDot, maxDot, dot2, dotSet)
+
+    # 划分上包右边的点集
+    rightSet, maxDot = maxSize(seq, dot3, dot4, dotSet)
+    # ----------------------------------------------------------------------
+
+    # 对上包右包的点集进一步划分
+    if rightSet:
+        divideUp(rightSet, dot3, maxDot, maxDot, dot4, dotSet)
+
+    return dotSet
 
 
-def drawpoint(point, color, line):
-    p = turtle.getturtle()
-    p.hideturtle()
-    turtle.delay(1)
-    if (line == 'p'):
-        p.speed(0)
-        for i in point:
-            p.pu()
-            p.color(color)
-            p.goto(i)
-            p.pd()
-            p.dot()
-    elif (line == 'l'):
-        p.pu()
-        p.speed(1)
-        for i in point:
-            p.color(color)
-            p.goto(i)
-            p.pd()
-            p.dot()
-        p.goto(point[0])
+# 下包的递归划分
+def divideDown(seq, dot1, dot2, dot3, dot4, dotSet=None):
+    # 初始化第一次运行时的参数
+    if len(seq) == 0:
+        return dotSet
+    if dotSet is None:
+        dotSet = []
+    if len(seq) == 1:
+        dotSet.append(seq[0])
+        return dotSet
+    leftSet, rightSet = [], []
+    # 划分下包左边的点集
+    leftSet, minDot = minSize(seq, dot1, dot2, dotSet)
+    # ----------------------------------------------------------------------
+
+    # 对下包的左包进行进一步划分
+    if leftSet:
+        divideDown(leftSet, dot1, minDot, minDot, dot2, dotSet)
+
+    # 划分下包右包的点集
+    rightSet, minDot = minSize(seq, dot3, dot4, dotSet)
+    # ----------------------------------------------------------------------
+
+    # 对下包的右包进一步划分
+    if rightSet:
+        divideDown(rightSet, dot3, minDot, minDot, dot4, dotSet)
+
+    return dotSet
 
 
-drawpoint(point, 'black', 'p')
-drawpoint(ep, 'red', 'l')
-time.sleep(1)
+# 递归主函数
+def mainDivide(seq):
+    # 将序列中的点按横坐标升序排序
+    seq.sort()
+    res = []
+    # 获取横坐标做大、最小的点及横坐标中位数
+    dot1 = seq[0]
+    dot2 = seq[-1]
+    seq1 = []
+    maxSize = float('-inf')
+    maxDot = ()
+    seq2 = []
+    minSize = float('inf')
+    minDot = ()
+    # med_x=(seq[len(seq)//2][0]+seq[-len(seq)//2-1][0])/2
+    # 对序列划分为直线dot1 dot2左右两侧的点集并找出两个点集的距直线最远点
+    for u in seq[1:-1]:
+        size = calTri(dot1, dot2, u)
+        if size > 0:
+            if size > maxSize:
+                if len(maxDot) > 0:
+                    seq1.append(maxDot)
+                maxSize = size
+                maxDot = u
+                continue
+            else:
+                seq1.append(u)
+        elif size < 0:
+            if size < minSize:
+                if len(minDot) > 0:
+                    seq2.append(minDot)
+                minSize = size
+                minDot = u
+                continue
+            else:
+                seq2.append(u)
+    print('seq1', seq1, maxDot)
+    print('seq2', seq2, minDot)
+    # 调用内建递归函数
+    res1 = divideUp(seq1, dot1, maxDot, maxDot, dot2)
+    res2 = divideDown(seq2, dot1, minDot, minDot, dot2)
+    if res1 is not None:
+        res.extend(res1)
+    if res2 is not None:
+        res.extend(res2)
+    for u in [dot for dot in [dot1, dot2, maxDot, minDot] if len(dot) > 0]:
+        res.append(u)
+    return res
+
+
+seq0 = [(random.randint(-1000, 1000), random.randint(-1000, 1000)) for x in range(20)]
+seq0 = list(set(seq0))
+res = mainDivide(seq0)
+print('res', sorted(res))
+plt.axis([-1100, 1100, -1100, 1100])
+plt.title("overview")
+plt.scatter([dot[0] for dot in seq0], [dot[1] for dot in seq0], color='black',marker='.')
+plt.scatter([dot[0] for dot in res], [dot[1] for dot in res], color='red',marker='o')
+plt.show()
